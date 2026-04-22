7389c49c2bde255a1e48d114873d950d13cc334f
124211211 GLEDSON PERDIVAL DE MOURA JUNIOR <gledson.perdival.de.moura.junior@lcc1-55.lcc.ufcg.edu.br>
2026-04-22 09:59:29 -0300

    adição de novos tipos

--- roteiro1-124211211.sql
+++ roteiro1-124211211.sql
@@ -1,11 +1,47 @@
 CREATE TABLE AUTOMOVEL(
-    placa VARCHAR(10) PRIMARY KEY,
+    placa CHAR(7) PRIMARY KEY,
+    tipo VARCHAR(20) NOT NULL,
+    marca VARCHAR(50) NOT NULL,
     modelo VARCHAR(50) NOT NULL,
-    ano INT NOT NULL
+    ano CHAR(4) NOT NULL
 );
 
 CREATE TABLE SEGURADO(
-    cpf VARCHAR(11) PRIMARY KEY,
+    cpf CHAR(11) PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    endereco VARCHAR(200) NOT NULL
+    endereco VARCHAR(200) NOT NULL,
+    telefone VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE PERITO(
+    cpf CHAR(11) PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE OFICINA(
+
+);
+
+CREATE TABLE SEGURO(
+    segurado_cpf CHAR(11) NOT NULL,
+    automovel_placa CHAR(7) NOT NULL,
+    PRIMARY KEY (segurado_cpf, automovel_placa),
+    data_inicio DATE NOT NULL,
+    data_fim DATE NOT NULL,
+);
+
+CREATE TABLE SINISTRO(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    segurado_cpf CHAR(11) NOT NULL,
+    automovel_placa CHAR(7) NOT NULL,
+    data_ocorrencia DATE NOT NULL,
+    descricao TEXT NOT NULL,
+);
+
+CREATE TABLE PERICIA(
+    
+);
+
+CREATE TABLE REPARO(
+
 );
